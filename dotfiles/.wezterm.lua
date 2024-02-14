27c61f393f5958c7b2d3f926a5dfcc1d570bca4a
Calvo <igorcalvob@gmail.com>
2024-02-14 14:22:12 -0300

    improved wezterm config

--- dotfiles/.wezterm.lua
+++ dotfiles/.wezterm.lua
@@ -14,6 +14,19 @@ config.color_scheme = 'Tinacious Design (Dark)'
 config.font = wezterm.font 'JetBrains Mono'
 config.max_fps = 240
 config.animation_fps = 120
+config.prefer_egl = true
+-- ctrl + shift + l
+-- wezterm.gui.enumerate_gpus()
+config.webgpu_preferred_adapter = {
+  backend = "Vulkan",
+  device = 7812,
+  device_type = "DiscreteGpu",
+  driver = "NVIDIA",
+  driver_info = "525.147.05",
+  name = "NVIDIA GeForce RTX 2070 SUPER",
+  vendor = 4318,
+}
+config.front_end = "WebGpu"
 config.webgpu_power_preference = "HighPerformance"
 config.show_update_window = true
 config.default_prog = { 'fish' }
